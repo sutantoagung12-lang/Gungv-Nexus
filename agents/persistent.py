0670549e97fbfac9984718c63fdd7e87b7b011c1
sutantoagung12-lang <sutantoagung12@gmail.com>
2026-09-24 21:09:03 +0700

    Nexus 6-10 foundation

--- agents/persistent.py
+++ agents/persistent.py
@@ -1,3 +1,4 @@
+"""Bounded persistent-agent adapter."""
 class PersistentAgent:
     def __init__(self,runtime): self.runtime=runtime
     def run_once(self,objective,task): return self.runtime.cognitive_cycle(objective,task)
