b3d47ef127e713ee05aefc27bef3320368b78dfc
sutantoagung12-lang <sutantoagung12@gmail.com>
2026-09-24 21:09:25 +0700

    Nexus 11-15 foundation

--- simulation/world.py
+++ simulation/world.py
@@ -1,2 +1,3 @@
+"""World-model impact simulation."""
 class WorldModel:
     def simulate(self,state,change): return {'state':state,'proposed_change':change,'risk':'UNKNOWN','requires_evaluation':True}
