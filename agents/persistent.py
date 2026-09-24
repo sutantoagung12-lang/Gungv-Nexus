a29a9df4180b4a42102173b192d044c17087b4b3
sutantoagung12-lang <sutantoagung12@gmail.com>
2026-09-24 21:06:12 +0700

    Build Nexus 6-18 architecture

---
+++ agents/persistent.py
@@ -0,0 +1,5 @@
+"""Persistent agent supervisor."""
+class PersistentAgent:
+    def __init__(self,runtime): self.runtime=runtime
+    def run_once(self,objective,task):
+        return self.runtime.cognitive_cycle(objective,task)
