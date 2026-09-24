f33cf0df020d5123a249022c5f343cc59819ab68
sutantoagung12-lang <sutantoagung12@gmail.com>
2026-09-24 21:09:32 +0700

    Nexus 11-15 foundation

--- evolution/promotion.py
+++ evolution/promotion.py
@@ -1,2 +1,3 @@
+"""Controlled promotion gate."""
 class PromotionGate:
     def assess(self,experiment_passed,tests_passed): return {'eligible':bool(experiment_passed and tests_passed),'requires_review':True}
