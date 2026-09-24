50149123144a5f68cc004d932e0d104ad91de89c
sutantoagung12-lang <sutantoagung12@gmail.com>
2026-09-24 21:07:45 +0700

    Implement Nexus 6-18 foundations

--- agents/persistent.py
+++ agents/persistent.py
@@ -1,5 +1,3 @@
-"""Persistent agent supervisor."""
 class PersistentAgent:
     def __init__(self,runtime): self.runtime=runtime
-    def run_once(self,objective,task):
-        return self.runtime.cognitive_cycle(objective,task)
+    def run_once(self,objective,task): return self.runtime.cognitive_cycle(objective,task)
