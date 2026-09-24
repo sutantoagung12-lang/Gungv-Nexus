670377c2ce06a6eb43f56548fe760e4f8fc42e8a
sutantoagung12-lang <sutantoagung12@gmail.com>
2026-09-24 21:09:28 +0700

    Nexus 11-15 foundation

--- evolution/meta.py
+++ evolution/meta.py
@@ -1,2 +1,3 @@
+"""Meta-evaluation of system results."""
 class MetaEvaluator:
-    def inspect(self,result): return {'has_result':bool(result),'improvement_candidate':True}
+    def inspect(self,result): return {'has_result':bool(result),'improvement_candidate':bool(result),'requires_review':True}
